3fea4112a25f5308a4fa9442799efebd8f981f26
An Unsocial Pigeon <66815528+AnUnsocialPigeon@users.noreply.github.com>
2022-03-31 22:49:41 +0100

    Code juggle

--- Goerge Timetable/MainWindow.xaml.cs
+++ Goerge Timetable/MainWindow.xaml.cs
@@ -104,26 +104,6 @@ namespace Exam_Countdown {
             Close();
         }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-        [DllImportAttribute("user32.dll")]
-        public static extern bool ReleaseCapture();
-
-        //Attach this to the MouseDown event of your drag control to move the window in place of the title bar
-        private void WindowDrag(object sender, MouseButtonEventArgs e) // MouseDown
-        {
-            ReleaseCapture();
-            SendMessage(new WindowInteropHelper(this).Handle,
-                0xA1, (IntPtr)0x2, (IntPtr)0);
-        }
-
-        //Attach this to the PreviewMousLeftButtonDown event of the grip control in the lower right corner of the form to resize the window
-        private void WindowResize(object sender, MouseButtonEventArgs e) //PreviewMousLeftButtonDown
-        {
-            HwndSource hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
-            SendMessage(hwndSource.Handle, 0x112, (IntPtr)61448, IntPtr.Zero);
-        }
 
         private void TextPosChange(object sender, RoutedEventArgs e) => MainTXTBlock.TextAlignment = (TextAlignment)int.Parse((sender as Button).Tag.ToString());
 
@@ -166,5 +146,30 @@ namespace Exam_Countdown {
         }
 
         private void SaveBTN_Click(object sender, RoutedEventArgs e) => SaveConfigFile();
+
+
+
+        #region moving
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        //Attach this to the MouseDown event of your drag control to move the window in place of the title bar
+        private void WindowDrag(object sender, MouseButtonEventArgs e) // MouseDown
+        {
+            ReleaseCapture();
+            SendMessage(new WindowInteropHelper(this).Handle,
+                0xA1, (IntPtr)0x2, (IntPtr)0);
+        }
+
+        //Attach this to the PreviewMousLeftButtonDown event of the grip control in the lower right corner of the form to resize the window
+        private void WindowResize(object sender, MouseButtonEventArgs e) //PreviewMousLeftButtonDown
+        {
+            HwndSource hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
+            SendMessage(hwndSource.Handle, 0x112, (IntPtr)61448, IntPtr.Zero);
+        }
+        #endregion
     }
 }
